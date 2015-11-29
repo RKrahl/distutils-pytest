7410eb39fc45fb2cea396f787e62be51a28ff801
Rolf Krahl <rolf@rotkraut.de>
2015-11-29 21:18:25 +0100

    Initial version.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python
+
+from distutils.core import setup
+import distutils_pytest
+
+setup(
+    name = "distutils-pytest",
+    version = distutils_pytest.__version__,
+    author = "Rolf Krahl",
+    author_email = "rolf@rotkraut.de",
+    license = "Apache-2.0",
+    py_modules = ["distutils_pytest"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Build Tools",
+    ],
+    cmdclass = {'test': distutils_pytest.test},
+)
