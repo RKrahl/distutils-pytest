eb6b9f81ade3e645844364bd1cbbe75a1f2ba502
Rolf Krahl <rolf@rotkraut.de>
2022-08-01 23:28:04 +0200

    Drop support for Python older than 3.4

--- setup.py
+++ setup.py
@@ -14,13 +14,6 @@ setup(
     py_modules = ["distutils_pytest"],
     classifiers = [
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Development Status :: 4 - Beta",
